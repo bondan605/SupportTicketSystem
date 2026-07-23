ae8d7f7eb5ea69096dedd49f8fe1f60404395e46
Muhammad Azwar Anas <tuangKarombang@gmail.com>
2026-07-23 17:57:21 +0700

    Update entity Tiket

--- src/05.Infrastructure/Persistence/Configurations/TicketConfiguration.cs
+++ src/05.Infrastructure/Persistence/Configurations/TicketConfiguration.cs
@@ -20,8 +20,8 @@ namespace SupportTicketSystem.Infrastructure.Persistence.Configurations
             entity.Property(e => e.TicketNumber)
                 .IsRequired()
                 .HasMaxLength(10);
-            entity.HasIndex(e => e.TicketNumber)
-                .IsUnique(); // Business Rule: Unique TKT-XXXXX
+            //entity.HasIndex(e => e.TicketNumber)
+            //    .IsUnique(); // Business Rule: Unique TKT-XXXXX
 
             entity.Property(e => e.CustomerName)
                 .IsRequired()
@@ -42,6 +42,7 @@ namespace SupportTicketSystem.Infrastructure.Persistence.Configurations
             entity.Property(e => e.Status)
                 .HasConversion<string>()
                 .HasMaxLength(20)
+                .HasDefaultValue(Domain.Enums.TicketStatus.Open)
                 .IsRequired();
 
             // One-to-Many Relationship: User (Manager/Agent) to Tickets
@@ -55,6 +56,8 @@ namespace SupportTicketSystem.Infrastructure.Persistence.Configurations
                 .WithOne(th => th.Ticket)
                 .HasForeignKey(th => th.TicketId)
                 .OnDelete(DeleteBehavior.Cascade);
+
+            entity.HasIndex(t => new { t.CreatedDate, t.Status, t.AssignedTo });
 
             // Data Seeding
             // Seed Data
--- src/06.WebApi/Controllers/TicketsController.cs
+++ src/06.WebApi/Controllers/TicketsController.cs
@@ -1,4 +1,5 @@
 ﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SupportTicketSystem.Application.Abstractions.Services;
@@ -10,6 +11,7 @@ using System.Linq;
 
 namespace SupportTicketSystem.WebApi.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route(ApiRoutes.Tickets.Base)]
     [Produces("application/json")]
