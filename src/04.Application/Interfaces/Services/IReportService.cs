5eaa4a257a1e03413ee816e35bb850ef19192f04
yashiruu <yasir112358@gmail.com>
2026-07-27 23:23:18 +0700

    refactor(report): remove report feature by Azwar

--- src/04.Application/Interfaces/Services/IReportService.cs
+++ src/04.Application/Interfaces/Services/IReportService.cs
@@ -1,10 +1,6 @@
-﻿using SupportTicketSystem.Shared.Dtos.Reports;
-using SupportTicketSystem.Shared.Models;
-
-namespace SupportTicketSystem.Application.Interfaces;
+﻿namespace SupportTicketSystem.Application.Interfaces;
 
 public interface IReportService
 {
-    Task<PagedResult<ManagerReportItemDto>> GetManagerReportAsync(ManagerReportFilterDto filter);
-    Task<DashboardSummaryDto> GetDashboardSummaryAsync();
+
 }
